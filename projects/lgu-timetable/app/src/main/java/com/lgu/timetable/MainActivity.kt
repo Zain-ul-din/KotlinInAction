99e8b92da58ce9d35b9b34b6deca3fbd0bc42592
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2023-10-14 16:30:45 +0500

    moved: files to dest folder #2

--- projects/lgu-timetable/app/src/main/java/com/lgu/timetable/MainActivity.kt
+++ projects/lgu-timetable/app/src/main/java/com/lgu/timetable/MainActivity.kt
@@ -32,6 +32,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun HelloWorld () {
     LGUTimetableTheme {
-        Text(text = "hello world")
+        Text(text = "hello world!")
     }
 }
